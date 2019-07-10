f4827a25c2c96041d6ea6a7495139b28df71f510
Leszek Gonczar <gonczar@gmail.com>
2019-07-10 10:55:09 +0100

    Add a class with OCR data field names (#9)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/model/OcrFieldNames.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.bulkscanccdeventhandler.model;
+
+public final class OcrFieldNames {
+
+    public static final String LEGACY_ID = "legacy_id";
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String DATE_OF_BIRTH = "date_of_birth";
+    public static final String CONTACT_NUMBER = "contact_number";
+    public static final String EMAIL = "email";
+    public static final String ADDRESS_LINE_1 = "address_line_1";
+    public static final String ADDRESS_LINE_2 = "address_line_2";
+    public static final String ADDRESS_LINE_3 = "address_line_3";
+    public static final String POST_CODE = "post_code";
+    public static final String POST_TOWN = "post_town";
+    public static final String COUNTY = "county";
+    public static final String COUNTRY = "country";
+
+    private OcrFieldNames() {
+        // utility class, not to be instantiated
+    }
+}
