53b89d3129b6723341a3500b6764baa70f7bb4c4
aliveni <aliveni.ch@gmail.com>
2019-07-30 12:14:34 +0100

    Functional tests config (#37)

--- infrastructure/test_config.tf
+++ infrastructure/test_config.tf
@@ -4,8 +4,8 @@ data "azurerm_key_vault_secret" "source_test_s2s_secret" {
   name         = "microservicekey-bulk-scan-sample-app-tests"
 }
 
-resource "azurerm_key_vault_secret" "test_s2s_secret" {
+resource "azurerm_key_vault_secret" "bulk_scan_sample_app_tests_s2s_secret" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "test-s2s-secret"
+  name         = "bulk-scan-sample-app-tests-s2s-secret"
   value        = "${data.azurerm_key_vault_secret.source_test_s2s_secret.value}"
 }
