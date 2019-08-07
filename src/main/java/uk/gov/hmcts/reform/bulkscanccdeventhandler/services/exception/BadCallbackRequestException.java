6501b01219a91a6e264defbfce7795aabf5382ae
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2019-08-07 16:42:56 +0100

    Remove no longer needed ccd callback handling (#50)

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/services/exception/BadCallbackRequestException.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.hmcts.reform.bulkscanccdeventhandler.services.exception;
-
-// TODO: map to 400 response when the exception handler is added
-public class BadCallbackRequestException extends RuntimeException {
-
-    public BadCallbackRequestException(String message) {
-        super(message);
-    }
-}
--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/services/exception/CcdDataParseException.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.hmcts.reform.bulkscanccdeventhandler.services.exception;
-
-public class CcdDataParseException extends RuntimeException {
-
-    public CcdDataParseException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
