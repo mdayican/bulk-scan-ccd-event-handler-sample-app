0673063f58457730f3772401ac8ee53d3b35fcd6
donatas M <1420191+doncem@users.noreply.github.com>
2019-08-14 11:44:03 +0100

    Prepare functional test suite for more test cases (#58)
    * Move config load to `TestHelper` constructor
    
    * Convert method to static
    
    * Move relaxed https validation to rest assured config
    
    * Move generic post function to `TestHelper`
    
    * Remove not necessary accessor

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrFormValidationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrFormValidationTest.java
@@ -1,21 +1,15 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import io.restassured.RestAssured;
 import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.ocrvalidation.model.out.OcrValidationResponse;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.ocrvalidation.model.out.ValidationStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
 @ExtendWith(SpringExtension.class)
 @TestPropertySource("classpath:application.conf")
@@ -23,24 +17,7 @@ public class OcrFormValidationTest {
 
     private static final String FORM_TYPE_PERSONAL = "PERSONAL";
 
-    private String testUrl;
-
-    private String s2sUrl;
-
-    private String s2sName;
-
-    private String s2sSecret;
-
     private final TestHelper testHelper = new TestHelper();
-
-    @BeforeEach
-    public void setUp() {
-        Config config = ConfigFactory.load();
-        this.testUrl = config.getString("test-url");
-        this.s2sUrl = config.getString("test-s2s-url");
-        this.s2sName = config.getString("test-s2s-name");
-        this.s2sSecret = config.getString("test-s2s-secret");
-    }
 
     @Test
     public void should_validate_ocr_data_and_return_success() {
@@ -99,15 +76,9 @@ public class OcrFormValidationTest {
     }
 
     private Response sendOcrFormValidationRequest(String formType, String fileName) {
-        return RestAssured
-            .given()
-            .relaxedHTTPSValidation()
-            .header("ServiceAuthorization", "Bearer " + testHelper.s2sSignIn(s2sName, s2sSecret, s2sUrl))
-            .header(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .baseUri(testUrl)
-            .body(testHelper.fileContentAsBytes(fileName))
-            .when()
-            .post("/forms/" + formType + "/validate-ocr")
-            .andReturn();
+        return testHelper.postWithBody(
+            "/forms/" + formType + "/validate-ocr",
+            TestHelper.fileContentAsBytes(fileName)
+        );
     }
 }
--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/TestHelper.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/TestHelper.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.reform.bulkscanccdeventhandler;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import com.warrenstrange.googleauth.GoogleAuthenticator;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 import java.util.Map;
@@ -13,10 +16,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-public class TestHelper {
+class TestHelper {
 
-    protected String s2sSignIn(String s2sName, String s2sSecret, String s2sUrl) {
+    private final String testUrl;
 
+    private final String s2sUrl;
+
+    private final String s2sName;
+
+    private final String s2sSecret;
+
+    TestHelper() {
+        Config config = ConfigFactory.load();
+
+        this.testUrl = config.getString("test-url");
+        this.s2sUrl = config.getString("test-s2s-url");
+        this.s2sName = config.getString("test-s2s-name");
+        this.s2sSecret = config.getString("test-s2s-secret");
+
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    private String s2sSignIn() {
         Map<String, Object> params = ImmutableMap.of(
             "microservice", s2sName,
             "oneTimePassword", new GoogleAuthenticator().getTotpPassword(s2sSecret)
@@ -24,7 +45,6 @@ public class TestHelper {
 
         Response response = RestAssured
             .given()
-            .relaxedHTTPSValidation()
             .baseUri(s2sUrl)
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .body(params)
@@ -39,7 +59,19 @@ public class TestHelper {
             .print();
     }
 
-    protected byte[] fileContentAsBytes(String file) {
+    Response postWithBody(String location, byte[] body) {
+        return RestAssured
+            .given()
+            .header("ServiceAuthorization", "Bearer " + s2sSignIn())
+            .header(CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .baseUri(testUrl)
+            .body(body)
+            .when()
+            .post(location)
+            .andReturn();
+    }
+
+    static byte[] fileContentAsBytes(String file) {
         try {
             return Resources.toByteArray(Resources.getResource(file));
         } catch (IOException e) {
